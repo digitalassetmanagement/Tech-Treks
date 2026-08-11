222a07ed456395a865e4f4ecaea89f7a79322f47
digitalassetmanagement <digitalassetmanagement@simplilearn.net>
2026-08-11 12:21:49 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,43 @@
+const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    category: "Electronics",
+    price: 65000,
+    status: "Available",
+  },
+
+  {
+    id: 2,
+    name: "Headphones",
+    category: "Electronics",
+    price: 2500,
+    status: "Available",
+  },
+
+  {
+    id: 3,
+    name: "Office Chair",
+    category: "Furniture",
+    price: 7000,
+    status: "Out Of Stock",
+  },
+
+  {
+    id: 4,
+    name: "Keyboard",
+    category: "Electronics",
+    price: 1200,
+    status: "Available",
+  },
+
+  {
+    id: 5,
+    name: "Coffee Mug",
+    category: "Kitchen",
+    price: 350,
+    status: "Available",
+  },
+];
+
+export default products;
